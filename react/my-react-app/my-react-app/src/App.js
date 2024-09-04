4802db760436cd04b1ed2a44b15f001a792fe449
wishu92 <kimso339@naver.com>
2024-09-04 12:23:27 +0900

    reacvt

--- react/my-react-app/my-react-app/src/App.js
+++ react/my-react-app/my-react-app/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Post from './Post';
+import { useState } from 'react';
 
 function App() {
   const element = <h1 className='title'>안녕하세요 h1입니다</h1>
@@ -15,7 +16,23 @@ function App() {
       text : '김치찌개'
     }]
 
-    
+    const [ number , setNumber ] = useState(0)
+    const handleIncressNumber = () => {
+      setNumber(number+1)
+    }
+
+    const [text,setText] = useState ('')
+    const handlechangeText = (e) => {
+      setText(e.target.value)
+    }
+
+    const [inputText, setInputText] = useState('');
+    const inputFocus = useRef()
+    const handleChangeTextToButton = () => {
+      setInputText(inputFocus.current.value)
+    }
+
+
   return (
     <div className="App">
       {element}
@@ -24,6 +41,16 @@ function App() {
       })}  아래와 같음, 
       data 배열 안에있는 title을 map은 반복문을 통해 가져옴*/}
 
+<span>{number}</span>
+      <button onClick={handleIncressNumber}>딸깍</button>
+
+      <input type="text" onChange={handleChangeText}/>
+      <p>당신의 입력 : {text}</p>
+
+      <input type="text" ref={inputFocus}/>
+      <button onClick={handleChangeTextToButton}>인풋딸깍</button>
+      <p>버튼누르면 : {inputText}</p>
+
       {data.map((value,key)=>
         <p>{value.title}</p>
       )}
@@ -31,10 +58,9 @@ function App() {
       {data.map((value,key)=>
         <Post 제목={value.title} 내용={value.text}></Post>
       )}
-
-      
     </div>
-  );
+
+  )
 }
 
 export default App;
